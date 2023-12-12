9dba59cc27061ce29f4ea3d270e6e2d6e06c9992
ydonga2377 <ydonga2377@conestogac.on.ca>
2023-12-11 20:54:48 -0500

    fetching data

--- js/custom.js
+++ js/custom.js
@@ -27,3 +27,43 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$(document).ready(function() {
+    // Sample array of products
+    var products = [
+      { name: 'Shoes 1', price: 200, image: '../images/shoe1.png' },
+      { name: 'Shoes 2', price: 70, image: '../images/shoe2.png' },
+      { name: 'Shoes 3', price: 400, image: '../images/shoe3.png' },
+      { name: 'Shoes 4', price: 450, image: '../images/shoe4.png' },
+    ];
+
+    // Function to create product elements
+    function createProductElement(product) {
+      return `
+        <div class="col-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="${product.image}" alt="${product.name}">
+              </div>
+              <div class="detail-box">
+                <h6>${product.name}</h6>
+                <h6>
+                  Price
+                  <span>${product.price}</span>
+                </h6>
+              </div>
+              <div class="new">
+                <span>New</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      `;
+    }
+
+    // Loop through the products and append them to the productContainer
+    for (var i = 0; i < products.length; i++) {
+      $('#productContainer').append(createProductElement(products[i]));
+    }
+  });
