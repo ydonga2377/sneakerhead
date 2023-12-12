271a75ca3d43f8d6796da6bce1cd0d867adee5c5
ydonga2377 <ydonga2377@conestogac.on.ca>
2023-12-12 03:25:21 -0500

    home product slider functionality finished

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,15 @@
 $(document).ready(function() {
     // Sample array of products
     
-
-    // Function to create product elements
-    function createProductElement(product) {
-      return `
-        <div class="col-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="${product.image}" alt="${product.name}">
-              </div>
-              <div class="detail-box">
-                <h6>${product.name}</h6>
-                <h6>
-                  Price
-                  <span>${product.price}</span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>New</span>
-              </div>
-            </a>
-          </div>
-        </div>
-      `;
-    }
-
-    // Loop through the products and append them to the productContainer
-    for (var i = 0; i < products.length; i++) {
-      $('#productContainer').append(createProductElement(products[i]));
-    }
-    $('#sampleCarousel').owlCarousel({
+    $('#productCarousel').owlCarousel({
         loop: true,
         margin: 10,
         responsiveClass: true,
+        autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    nav:true,
+    dots:true,
         responsive: {
           0: {
             items: 1,
@@ -48,24 +23,32 @@ $(document).ready(function() {
         },
       });
   
-      // Sample data for 4 boxes
-      var sampleData = [
-        { content: 'Box 1' },
-        { content: 'Box 2' },
-        { content: 'Box 3' },
-        { content: 'Box 4' },
-      ];
-  
-      // Loop through the sample data and append them to the Owl Carousel
-      for (var i = 0; i < sampleData.length; i++) {
-        $('#sampleCarousel').trigger('add.owl.carousel', [createBoxElement(sampleData[i])]).trigger('refresh.owl.carousel');
+      // Loop through the first 4 products and append them to the Owl Carousel
+      for (var i = 0; i < 6; i++) {
+        $('#productCarousel').trigger('add.owl.carousel', [createProductElement(products[i])]).trigger('refresh.owl.carousel');
       }
   
-      // Function to create box elements
-      function createBoxElement(box) {
+      // Function to create product elements
+      function createProductElement(product) {
         return `
           <div class="item">
-            ${box.content}
+            <div class="box">
+              <a href="">
+                <div class="img-box">
+                  <img src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="detail-box">
+                  <h6>${product.name}</h6>
+                  <h6>
+                    Price
+                    <span>${product.price}</span>
+                  </h6>
+                </div>
+                <div class="new">
+                  <span>New</span>
+                </div>
+              </a>
+            </div>
           </div>
         `;
       }
