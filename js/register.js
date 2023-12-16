5178554cd6e91ed7bc7cd71c37b3d2826095b66a
Vivek Dhorajiya <vivekdhorajiya9@gmail.com>
2023-12-16 01:33:31 -0500

    Login & Register fix the functionality error

--- js/register.js
+++ js/register.js
@@ -1,44 +1,73 @@
 "use strict"
 
-$("#register-btn").click(function () {
-    var firstName = $("#f_name").val();
-    var lastName = $("#l_name").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var confirmPassword = $("#confirm_password").val();
+const clearError = (inputId) => {
+    $(`${inputId} + span`).text("");
+};
 
+$("#firstName, #lastName, #email, #registerPassword, #confirmPassword").on("input", function () {
+    clearError(`#${this.id}`);
+});
+
+$("#register-btn").click(function () {
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var email = $("#email").val();
+    var password = $("#registerPassword").val();
+    var confirmPassword = $("#confirmPassword").val();
+
+    var nameRegex = /^[a-zA-Z]+$/;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{5,}$/;
+
+    if (!firstName && !nameRegex.test(firstName)) {
+        $("#firstName + span").text("Please enter your First name and it should only contain char.");
+    }
+
+    if (!lastName && !nameRegex.test(lastName)) {
+        $("#lastName + span").text("Please enter your Last name and it should only contain char.");
+    }
+
+    if (!email && !emailRegex.test(email)) {
+        $("#email + span").text("Please enter valid email address.(ex: youremail@gmail.com)");
+    }
+
+    if (!passwordRegex.test(password)) {
+        $("#registerPassword + span").text("Password must contain at least one letter and one number, and be at least 5 characters long.");
+        return;
+    }
 
     if (password !== confirmPassword) {
-        alert("Confirm Password Does not match with password.Please enter the matching password.");
+        $("#confirmPassword + span").text("Confirm Password does not match with Password. Please enter the matching password.");
         return;
     }
 
     var storedUser = JSON.parse(localStorage.getItem("users")) || [];
 
     var existingUser = storedUser.find(u => u.username === email);
-    
+
     if (existingUser) {
         alert("Email is already registered. Please try with new email address.");
-    }else {
+    } else {
         storedUser.push({ firstName: firstName, lastName: lastName, email: email, password: password });
         localStorage.setItem("users", JSON.stringify(storedUser));
         window.location.href = "index.html";
     }
+
 })
 
 const clearForm = () => {
 
-    $("#f_name").value = "";
-    $("#l_name").value = "";
-    $("#email").value = "";
-    $("#password").value = "";
-    $("#confirm_password").value = "";
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#email").val("");
+    $("#registerPassword").val("");
+    $("#confirmPassword").val("");
 
-    $("#fn_error").textContent = "";
-    $("#ln_error").textContent = "";
-    $("#email_error").textContent = "";
-    $("#pass_error").textContent = "";
-    $("#confirm_pass_error").textContent = "";
+    // $("#fn_error").text("");
+    // $("#ln_error").text("");
+    // $("#email_error").text("");
+    // $("#pass_error").text("");
+    // $("#confirm_pass_error").text("");
 
     $("#f_name").focus();
 };
